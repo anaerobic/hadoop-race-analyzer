bf7e0671c8c8f6c6125409adbfcf5bba28c9c4ad
Michael Martin <mwm.cal@gmail.com>
2015-05-12 16:15:42 -0700

    using bin/hdfs instead of bin/hadoop script

--- working/share/race-analyzer.sh
+++ working/share/race-analyzer.sh
@@ -2,11 +2,11 @@ cd $HADOOP_PREFIX
 
 bin/hdfs dfsadmin -safemode leave
 
-bin/hadoop dfs -mkdir /hdfs
+bin/hdfs dfs -mkdir /hdfs
 
-bin/hadoop dfs -copyFromLocal /share/data/race-results.json /hdfs/race-results.json
+bin/hdfs dfs -copyFromLocal /share/data/race-results.json /hdfs/race-results.json
 
-bin/hadoop fs -rm -r /hdfs-output
+bin/hdfs fs -rm -r /hdfs-output
 
 bin/hadoop jar /share/hadoop-race-analyzer.jar app.AnalyzeRace /hdfs /hdfs-output
 
